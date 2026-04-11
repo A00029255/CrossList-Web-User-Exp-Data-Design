c1151dc69661be71f0f1da256ecb7bc90eb38589
Andile-cpu <x00228810@mytudublin.ie>
2026-04-11 21:29:48 +0100

    did code clean up

---
+++ helper.js
@@ -0,0 +1,3 @@
+const supabaseUrl = "https://eehtvevnukdhrwudlmat.supabase.co";
+const supabaseKey = "sb_publishable_P9yhIU_CzvEePe9KujiJpg_zACInFVc";
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
